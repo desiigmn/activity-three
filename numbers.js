d3554ab0cc84351e36857aea262024c4f0d31854
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-09 08:30:22 +0800

    Completed Numbers Section

---
+++ numbers.js
@@ -0,0 +1,24 @@
+console.log("****NUMBERS****");
+let firstFavNum = 10; 
+let secondFavNum = 18; 
+
+
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulus = firstFavNum % secondFavNum;
+let exponentiation = firstFavNum ** secondFavNum;
+
+console.log("****ADDITION OPERATION****");
+console.log("10 + 18 = ", addition);
+console.log("****SUBTRACTION OPERATION****");
+console.log("10 - 18 = ", subtraction);
+console.log("****MULTIPLICATION OPERATION****");
+console.log("10 * 18 = ", multiplication);
+console.log("****DIVISION OPERATION****");
+console.log("10 / 18 = ", division);
+console.log("****MODULUS OPERATION****");
+console.log("10 % 18 = ", modulus);
+console.log("****EXPONENTIATION (POWER) OPERATION****");
+console.log("10 ** 18 = ", exponentiation);
